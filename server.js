3a345bcdf7cce24833956a0e67807670530c27a5
kkomaz <leeajl006@gmail.com>
2015-05-25 00:44:21 -0400

    generated index view chatroom

--- server.js
+++ server.js
@@ -3,13 +3,24 @@ var express = require('express');
     http = require("http").createServer(app);
     bodyParser = require('body-parser');
 
+//Server configuration
 app.set("ipaddr", "127.0.0.1");
 app.set("port", 8080);
+
+//Specify the views folder 
+//see if your can refactor with require path (**)
+app.set("views", __dirname + "/views");
+
+//View engine is in Jade
+app.set("view engine", "jade");
+
+//Specify where static content is
+app.use(express.static("public", __dirname + "/public"));
 
 app.use(bodyParser.json());
 
 app.get("/", function(request, response){
-  response.send("Server is up and running");
+  response.render("index");
 });
 
 http.listen(app.get("port"), app.get("ipaddr"), function(){
